c30ad856627417ef5d458069a0eb8d4a37d2ed0d
Anthony BARA <banthony@e2r11p18.42.fr>
2018-06-02 10:11:00 +0200

    cameraScript to follow the player, begin weapon class

---
+++ rush00/Assets/Script/CameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+    public GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.position = player.transform.position;
+        transform.Translate(new Vector3(0F, 0F, -10));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position;
+        transform.Translate(new Vector3(0F, 0F, -10));
+    }
+}
