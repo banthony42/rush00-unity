c0aee93731932a827c2a78af424c9d19d6c230e4
gwojda <gwojda@student.42.fr>
2018-06-02 14:52:03 +0200

    debut de la porte et des ennemis

---
+++ rush00/Assets/Script/DoorPhysics.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorPhysics : MonoBehaviour {
+
+	private const float	smooth = 2.0f;
+	private const float	DoorOpenAngle = 90.0f;
+	private const float	DoorCloseAngle = 0.0f;
+
+	private float	direction = 1.0f;
+	private bool	open = false;
+
+	void Update ()
+	{
+		if (open == true)
+		{
+			Quaternion target = Quaternion.Euler(DoorOpenAngle, 0, 0);
+			transform.localRotation = Quaternion.Slerp(transform.localRotation, target, Time.deltaTime * smooth);
+		}
+		else
+		{
+			Quaternion target = Quaternion.Euler(DoorCloseAngle, 0, 0);
+			transform.localRotation = Quaternion.Slerp(transform.localRotation, target, Time.deltaTime * smooth);
+		 }
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Character" || other.gameObject.tag == "Player")
+		{
+			open = true;
+			StartCoroutine("CloseDoor");
+		}
+	}
+
+	IEnumerator CloseDoor()
+    {
+        yield return new WaitForSeconds(5);
+		open = false;
+    }
+
+}
